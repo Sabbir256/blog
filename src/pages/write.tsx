db8bf19e2375cbdfd805a2e3123734411713b906
sabbir <29686831+aboutsabbir@users.noreply.github.com>
2022-10-15 15:44:58 +0600

    designing form

--- src/pages/write.tsx
+++ src/pages/write.tsx
@@ -1,7 +1,25 @@
+import { TextField, Button } from "@mui/material";
+
 export default function Write(){
     return(
-        <div>
-            This will return a form.
+        <div className="w-4/5 ml-auto mr-auto">
+            <h1 className="text-2xl mt-9">Write a new Blog!</h1>
+            <form id="newBlog" className="mt-6 grid grid-cols-1 gap-y-8">
+                <TextField 
+                    id="title"
+                    label="Title" 
+                    variant="standard"
+                    placeholder="A title for the blog"/>
+                
+                <TextField
+                    id="content"
+                    label="Content"
+                    variant="standard"
+                    multiline
+                    placeholder="Here goes the blog content"/>
+
+                <Button variant="contained">Submit</Button>
+            </form>
         </div>
     );
 }
