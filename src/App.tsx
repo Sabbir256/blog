594452ccedba48f6244f26aab9fd1097191f07e6
sabbir <29686831+aboutsabbir@users.noreply.github.com>
2022-10-15 14:37:42 +0600

    add tailwind to the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
 import './App.css'
+import Blog from './components/blog'
 
-function Hello(){
-  return (
-    <div>
-      Hey! Welcome to my blog!
-    </div>
-  );
-}
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <Hello />
+      <Blog/>
     </div>
   )
 }
---
+++ src/components/blog.tsx
@@ -0,0 +1,9 @@
+function Blog(){
+    return(
+        <div className="text-green-700 py-4 text-center">
+            This is the main blog body!
+        </div>
+    );
+}
+
+export default Blog;
