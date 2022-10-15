2f06a8bdbb38a4598312aef3c97f9155a8c7e4b5
sabbir <29686831+aboutsabbir@users.noreply.github.com>
2022-10-15 14:56:49 +0600

    adding routes

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Write from './pages/write'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<App/>} />
+        <Route path='/writeBlog' element={<Write/>} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 )
---
+++ src/pages/write.tsx
@@ -0,0 +1,7 @@
+export default function Write(){
+    return(
+        <div>
+            This will return a form.
+        </div>
+    );
+}
